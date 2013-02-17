119bc5188016ea3543b022c02802804da496db40
Gediminas <gediminas.backevicius@gmail.com>
2013-02-17 21:39:23 +0200

    Documentation was updated

---
+++ examples/object_initializer.js
@@ -0,0 +1,24 @@
+var Shape = $class({
+  __construct__: function () {
+    this.__x = 0;
+    this.__y = 0;
+  },
+  x: {
+    get: function () {
+      return this.__x;
+    },
+    set: function (value) {
+      this.__x = value;
+    }
+  },
+  y: {
+    get: function () {
+      return this.__y;
+    },
+    set: function (value) {
+      this.__y = value;
+    }
+  }
+});
+
+var shape = new Shape({ x: 100, y: 100 });
