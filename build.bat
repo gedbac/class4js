b7254f4b7f0bb34be8d81fdec3760ccd3f621384
Gediminas Backevicius <gediminas.backevicius@gmail.com>
2013-06-03 09:36:31 +0300

    Module's building script for node.js was fixed

--- build.bat
+++ build.bat
@@ -61,28 +61,10 @@ call :test-browser
 call :test-phantomjs
 goto :exit
 
-:build
-echo var class4js = (function (global) {>> .\class4js.js
-echo.>> .\class4js.js
-echo var exports = {};>> .\class4js.js
-echo.>> .\class4js.js
-%NODE% .\build\cat.js %SOURCES_FILES%>> .\class4js.js
-echo.>> .\class4js.js
-echo return exports;>> .\class4js.js
-echo.>> .\class4js.js
-echo }(typeof global !== 'undefined' ? global : window));>> .\class4js.js
-echo.>> .\class4js.js
-echo if (typeof module !== 'undefined' ^&^& module !== null) {>> .\class4js.js
-echo   module.exports = class4js;>> .\class4js.js
-echo }>> .\class4js.js
-goto :exit
-
 :build-node
 call :clean-node
-call :build
+call :build-browser
 copy .\class4js.js .\lib\class4js.js
-%NODE% .\build\cat.js %SOURCES_FILES% >> .\lib\class4js.js
-%NODE% .\build\include_strict_mode.js .\lib\class4js.js
 goto :exit
 
 :clean-node
@@ -115,9 +97,22 @@ goto :exit
 
 :build-browser
 call :clean-browser
-call :build
-%JAVA% -jar ./build/yuicompressor-2.4.8.jar --type js --nomunge --preserve-semi --disable-optimizations .\class4js.js -o .\class4js.min.js
+echo var class4js = (function (global) {>> .\class4js.js
+echo.>> .\class4js.js
+echo var exports = {};>> .\class4js.js
+echo.>> .\class4js.js
+%NODE% .\build\cat.js %SOURCES_FILES% >> .\class4js.js
+echo.>> .\class4js.js
+echo.>> .\class4js.js
+echo return exports;>> .\class4js.js
+echo.>> .\class4js.js
+echo }(typeof global !== 'undefined' ? global : window));>> .\class4js.js
+echo.>> .\class4js.js
+echo if (typeof module !== 'undefined' ^&^& module !== null) {>> .\class4js.js
+echo   module.exports = class4js;>> .\class4js.js
+echo }>> .\class4js.js
 %NODE% .\build\include_strict_mode.js .\class4js.js
+%JAVA% -jar ./build/yuicompressor-2.4.8.jar --type js --nomunge --preserve-semi --disable-optimizations .\class4js.js -o .\class4js.min.js
 goto :exit
 
 :clean-browser
